2d149ef5daebbaf1cbdfea06514a50c8bd7737c4
Messan-Kodzo-Akotia <167632771+Messan-Kodzo-Akotia@users.noreply.github.com>
2024-04-23 15:20:31 +0000

    started analysis

---
+++ main.R
@@ -0,0 +1,7 @@
+library(tidyverse)
+library(here)
+tv_hours_table<- gss_cat %>%
+  group_by(marital)%>%
+  summarise(mean_tv_hours=mean(tvhours, na.rm = T))
+
+write.csv(tv_hours_table, here("TV_hours_by_Marital.csv"))
