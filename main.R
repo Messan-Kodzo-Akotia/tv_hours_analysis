e2a5877aa56954923e8dc2bfb5a3d1c7fd91a1cc
Messan-Kodzo-Akotia <167632771+Messan-Kodzo-Akotia@users.noreply.github.com>
2024-04-24 09:55:01 +0000

    make names longe

--- main.R
+++ main.R
@@ -1,8 +1,13 @@
 library(tidyverse)
 library(here)
+<<<<<<< Updated upstream
+tv_hours_tabulation<- gss_cat %>%
+  filter(age < 30)%>%
+=======
 tv_hours_table<- gss_cat %>%
-  filter(age < 30)
+  filter(age>= 30)%>%
+>>>>>>> Stashed changes
   group_by(marital)%>%
   summarise(mean_tv_hours=mean(tvhours, na.rm = T))
 
-write.csv(tv_hours_table, here("TV_hours_by_Marital.csv"))
+write.csv(tv_hours_tabulation, here("TV_hours_by_Marital.csv"))
